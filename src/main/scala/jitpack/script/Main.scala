62f3e932c056dc6a473458ff5e5c5a79669bbf78
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2019-03-25 17:19:48 +0530

    testing2

--- src/main/scala/jitpack/script/Main.scala
+++ src/main/scala/jitpack/script/Main.scala
@@ -1,6 +1,13 @@
 package jitpack.script
 
+import java.util
+
 object Main extends App{
+  val locations = util.Set.of(
+    "Networks.apply.hostname1",
+    "Networks.apply.hostname2",
+    "Networks.apply.hostname3")
+  
   println("Hello this is just a test")
 
 }
